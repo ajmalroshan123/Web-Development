6518b25cc4bed15acb054b0ff365267df8200c8a
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-07-13 16:46:37 +0530

    [feat] js datatypes

---
+++ javascript/datatype.js
@@ -0,0 +1,32 @@
+// let num = 10;
+// console.log(num);
+
+
+// Primitive Data Type
+
+// Number , String, Boolean, null, undefined, Symbol, bigint
+
+// Number
+let account_balance = 20.21;
+console.log( typeof account_balance)
+
+// String
+let name = "ajmal";
+console.log(typeof name);
+
+// Boolean 
+let statement = true;
+console.log(typeof statement)
+
+// undefined
+let obj;
+console.log(typeof obj);
+
+// Null
+let balance = null;
+console.log(typeof balance);
+// it will show object which is error in javascript , it is not corrected because of the legacy code written earlier
+
+// Bigint
+let number = 123456789098765432112345678900987654321n;
+console.log(typeof number);
