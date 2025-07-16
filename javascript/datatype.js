72d845f9b177ffc3c14830791102652d60708557
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-07-16 19:51:56 +0530

    [feat] javascript datatypes

--- javascript/datatype.js
+++ javascript/datatype.js
@@ -30,3 +30,43 @@ console.log(typeof balance);
 // Bigint
 let number = 123456789098765432112345678900987654321n;
 console.log(typeof number);
+
+
+
+// Non Primitive DataType
+// Array, Object, Functions
+
+// Array
+let arr = [10, 20, 30, "rohit"]
+console.log( typeof arr)
+
+
+// Object
+let user = {
+    name: "ajmal",
+    age: 23
+    }
+console.log(typeof user)    
+
+// Function
+let fun = function() {
+    return ("hello world")
+}
+console.log(fun());
+
+
+// conversion
+let account_number = '123321';
+console.log(typeof Number(account_number))
+
+let account = "100CSS"
+console.log(Number(account));  // NaN
+
+let num = 20;
+console.log(typeof String(num));
+
+let state = true;
+console.log(typeof state);
+
+console.log(typeof Number(state));
+
