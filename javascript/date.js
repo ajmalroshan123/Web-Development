c482364230cabc271c71cbb10f1ef485089812da
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-08-11 22:51:18 +0530

    feat: introduction to Date() in js

---
+++ javascript/date.js
@@ -0,0 +1,17 @@
+let d = new Date();
+console.log(d);
+
+
+console.log(d.getDate());
+console.log(d.getDay());
+console.log(d.getMonth());
+console.log(d.getFullYear());
+
+// get in milliseconds
+console.log(d.getTime());
+
+const now = Date.now();
+console.log(now);
+
+
+
