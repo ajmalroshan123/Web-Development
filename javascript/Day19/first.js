c9421e8f7a7d3a0635efb99d1e84e8c47cba9f28
ajmalroshan123 <ajmalroshan2002@gmail.com>
2025-08-30 22:06:05 +0530

    Dom Manipulation

---
+++ javascript/Day19/first.js
@@ -0,0 +1,13 @@
+// class Person{
+//     constructor{
+
+//     }
+// }
+// const obj = document.getElementById("first");
+// obj.className = "Mohan";
+
+
+
+console.log(window.document)
+
+document.getElementById('first').innerText = "Ajmal"
